8ab8c9a8bedb8d608d19590c60ebc95fe6eebb1f
Ronnel James Bote <ronneljames.miranda.bote@gmail.com>
2022-07-07 18:47:09 +0300

    Remove redundant canvas element check and throw errors

--- src/pages/examples/simple-triangle/index.tsx
+++ src/pages/examples/simple-triangle/index.tsx
@@ -8,56 +8,56 @@ import vertexShaderSource from './_shaders/vertex.glsl'
 
 const SimpleTriangle: NextPage = () => {
   useEffect(() => {
+    const canvasId = 'canvas'
     const canvasDimensions = 500
-    const canvasContainer = document.getElementById('canvas')
-    if (canvasContainer) {
-      const canvas: HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement
-      if (!canvas) {
-        return
-      }
-      canvas.width = canvasDimensions
-      canvas.height = canvasDimensions
-      canvas.style.width = `${canvas.width}px`
-      canvas.style.height = `${canvas.height}px`
 
-      const gl = canvas.getContext('webgl2')
-      if (!gl) {
-        return
-      }
-
-      const vertexShader = compileShader(gl, vertexShaderSource, gl.VERTEX_SHADER)
-      const fragmentShader = compileShader(gl, fragmentShaderSource, gl.FRAGMENT_SHADER)
-      const program = createProgram(gl, vertexShader, fragmentShader)
-
-      gl.useProgram(program)
-
-      gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
-      gl.clearColor(0, 0, 1, 1)
-      gl.clear(gl.COLOR_BUFFER_BIT)
-
-      // In pixels
-      const vertexA = [100, 50]
-      const vertexB = [100, 250]
-      const vertexC = [350, 50]
-      const vertices = new Float32Array([...vertexA, ...vertexB, ...vertexC])
-
-      const positionBuffer = gl.createBuffer()
-      gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
-      gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
-
-      const color = gl.getUniformLocation(program, 'u_color')
-      gl.uniform4fv(color, new Float32Array([1, 1, 0, 1]))
-
-      const resolution = gl.getUniformLocation(program, 'u_resolution')
-      gl.uniform1f(resolution, canvasDimensions)
-
-      const vertexArrayObject = gl.createVertexArray()
-      gl.bindVertexArray(vertexArrayObject)
-      const positionAttributeLocation = gl.getAttribLocation(program, 'a_position')
-      gl.enableVertexAttribArray(positionAttributeLocation)
-      gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0)
-      gl.drawArrays(gl.TRIANGLES, 0, vertices.length / 2)
+    const canvas: HTMLCanvasElement = document.getElementById(canvasId) as HTMLCanvasElement
+    if (!canvas) {
+      throw `Canvas element not found`
     }
+
+    canvas.width = canvasDimensions
+    canvas.height = canvasDimensions
+    canvas.style.width = `${canvas.width}px`
+    canvas.style.height = `${canvas.height}px`
+
+    const gl = canvas.getContext('webgl2')
+    if (!gl) {
+      throw `WebGL not supported`
+    }
+
+    const vertexShader = compileShader(gl, vertexShaderSource, gl.VERTEX_SHADER)
+    const fragmentShader = compileShader(gl, fragmentShaderSource, gl.FRAGMENT_SHADER)
+    const program = createProgram(gl, vertexShader, fragmentShader)
+
+    gl.useProgram(program)
+
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+    gl.clearColor(0, 0, 1, 1)
+    gl.clear(gl.COLOR_BUFFER_BIT)
+
+    // In pixels
+    const vertexA = [100, 50]
+    const vertexB = [100, 250]
+    const vertexC = [350, 50]
+    const vertices = new Float32Array([...vertexA, ...vertexB, ...vertexC])
+
+    const positionBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
+
+    const color = gl.getUniformLocation(program, 'u_color')
+    gl.uniform4fv(color, new Float32Array([1, 1, 0, 1]))
+
+    const resolution = gl.getUniformLocation(program, 'u_resolution')
+    gl.uniform1f(resolution, canvasDimensions)
+
+    const vertexArrayObject = gl.createVertexArray()
+    gl.bindVertexArray(vertexArrayObject)
+    const positionAttributeLocation = gl.getAttribLocation(program, 'a_position')
+    gl.enableVertexAttribArray(positionAttributeLocation)
+    gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0)
+    gl.drawArrays(gl.TRIANGLES, 0, vertices.length / 2)
   }, [])
 
   return <CanvasContainer title="Simple Triangle" />
