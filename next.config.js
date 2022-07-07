6dbf237d9c8c0dd62a0479dafc87cd627470d7af
Ronnel James Bote <ronneljames.miranda.bote@gmail.com>
2022-07-07 16:14:56 +0300

    Install webpack loader for shaders

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(glsl)$/,
+      use: [
+        {
+          loader: 'ts-shader-loader',
+        },
+      ],
+    })
+    return config
+  },
 }
 
 module.exports = nextConfig
