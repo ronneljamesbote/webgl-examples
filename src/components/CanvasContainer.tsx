25e42c76640d721f83958b55f5919a4709436520
Ronnel James Bote <ronneljames.miranda.bote@gmail.com>
2022-07-07 18:43:35 +0300

    Add reusable component for canvas container and head

---
+++ src/components/CanvasContainer.tsx
@@ -0,0 +1,37 @@
+import Head from 'next/head'
+import { ReactNode } from 'react'
+
+interface CanvasContainerProps {
+  title: string
+  canvasId?: string
+  headContents?: ReactNode
+  children?: ReactNode
+}
+
+const CanvasContainer = ({ title, headContents, children, canvasId = 'canvas' }: CanvasContainerProps) => {
+  return (
+    <>
+      <Head>
+        <title>WebGL Examples - {title}</title>
+        <link rel="icon" href="/favicon.ico" />
+        {headContents}
+      </Head>
+
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: '1rem',
+        }}
+      >
+        <h1 style={{ paddingBottom: '2rem' }}>{title}</h1>
+        <canvas id={canvasId}></canvas>
+        {children}
+      </div>
+    </>
+  )
+}
+
+export default CanvasContainer
