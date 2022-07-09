e9f2f3edb359fe7b68e54680ab9af05caafe19dd
Ronnel James Bote <ronneljames.miranda.bote@gmail.com>
2022-07-09 22:47:15 +0300

    Implement triangle translation

--- src/pages/examples/rainbow-triangle/index.tsx
+++ src/pages/examples/rainbow-triangle/index.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from 'next'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 import { CanvasContainer } from '../../../components'
 import { initializeInstance } from '../../../utils/webgl'
@@ -9,7 +9,29 @@ import vertexShaderSource from './_shaders/vertex.glsl'
 const canvasId = 'canvas'
 const canvasDimensions = 500
 
+const drawScene = (gl: WebGL2RenderingContext, program: WebGLProgram) => {
+  const triangleGeometry = new Float32Array([0, 0, 250, 0, 0, 250])
+
+  const positionsBuffer = gl.createBuffer()
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionsBuffer)
+  gl.bufferData(gl.ARRAY_BUFFER, triangleGeometry, gl.STATIC_DRAW)
+
+  const vertexArrayObject = gl.createVertexArray()
+  gl.bindVertexArray(vertexArrayObject)
+  const positionAttributeLocation = gl.getAttribLocation(program, 'a_position')
+  gl.enableVertexAttribArray(positionAttributeLocation)
+  gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0)
+  gl.drawArrays(gl.TRIANGLES, 0, triangleGeometry.length / 2)
+
+  gl.useProgram(null)
+  gl.bindBuffer(gl.ARRAY_BUFFER, null)
+  gl.bindVertexArray(null)
+  gl.disableVertexAttribArray(positionAttributeLocation)
+}
+
 const RainbowTriangle: NextPage = () => {
+  const [translation, setTranslation] = useState<{ x: number; y: number }>({ x: 0, y: 0 })
+
   useEffect(() => {
     const { gl, program } = initializeInstance({
       canvasId: canvasId,
@@ -23,20 +45,49 @@ const RainbowTriangle: NextPage = () => {
       },
     })
 
-    const triangle = new Float32Array([-1, -1, 1, 0, 0, 1])
-    const positionsBuffer = gl.createBuffer()
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionsBuffer)
-    gl.bufferData(gl.ARRAY_BUFFER, triangle, gl.STATIC_DRAW)
+    const resolutionUniform = gl.getUniformLocation(program, 'u_resolution')
+    gl.uniform1f(resolutionUniform, canvasDimensions)
 
-    const vertexArrayObject = gl.createVertexArray()
-    gl.bindVertexArray(vertexArrayObject)
-    const positionAttributeLocation = gl.getAttribLocation(program, 'a_position')
-    gl.enableVertexAttribArray(positionAttributeLocation)
-    gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0)
-    gl.drawArrays(gl.TRIANGLES, 0, triangle.length / 2)
-  }, [])
+    const translationUniform = gl.getUniformLocation(program, 'u_translation')
+    gl.uniform2f(translationUniform, translation.x, translation.y)
 
-  return <CanvasContainer canvasId={canvasId} title="Rainbow Triangle" />
+    drawScene(gl, program)
+  }, [translation])
+
+  return (
+    <CanvasContainer canvasId={canvasId} title="Rainbow Triangle">
+      <div style={{ padding: '2rem' }}>
+        <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '1rem' }}>
+          <label style={{ marginRight: '1rem', fontWeight: '700' }}>X</label>
+          <input
+            type="range"
+            min={0}
+            max={canvasDimensions}
+            value={translation.x}
+            onChange={(e) =>
+              setTranslation((prevState) => {
+                return { x: Number(e.target.value), y: prevState.y }
+              })
+            }
+          />
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '1rem' }}>
+          <label style={{ marginRight: '1rem', fontWeight: '700' }}>Y</label>
+          <input
+            type="range"
+            min={0}
+            max={canvasDimensions}
+            value={translation.y}
+            onChange={(e) =>
+              setTranslation((prevState) => {
+                return { x: prevState.x, y: Number(e.target.value) }
+              })
+            }
+          />
+        </div>
+      </div>
+    </CanvasContainer>
+  )
 }
 
 export default RainbowTriangle
