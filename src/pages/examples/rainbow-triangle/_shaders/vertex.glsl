baed5d9a6fa100b208e514c5af84bd93a73e2a86
Ronnel James Bote <ronneljames.miranda.bote@gmail.com>
2022-07-09 22:46:56 +0300

    Update shader to apply translation

--- src/pages/examples/rainbow-triangle/_shaders/vertex.glsl
+++ src/pages/examples/rainbow-triangle/_shaders/vertex.glsl
@@ -1,13 +1,21 @@
 #version 300 es
-in vec4 a_position;
+in vec2 a_position;
+
+uniform float u_resolution;
+uniform vec2 u_translation;
 
 out vec4 v_color;
 
 void main() {
-    gl_Position = a_position;
+    vec2 translated = a_position + u_translation;
+    vec2 valueA = translated / u_resolution;
+    vec2 valueB = valueA * 2.0;
+    vec2 valueC = valueB - 1.0;
+    vec4 position = vec4(valueC, 0 ,1);
+    gl_Position = position;
 
     // Convert from clip space to color space.
     // Clip space goes -1.0 to +1.0
     // Color space goes from 0.0 to 1.0
-    v_color = a_position * 0.5 + 0.5;
+    v_color = position * 0.5 + 0.5;
 }
