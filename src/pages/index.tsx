c7ddea27875d9f4963427ece54db5811468aa27d
Ronnel James Bote <ronneljames.miranda.bote@gmail.com>
2022-07-10 12:11:58 +0300

    Build index page to show links of example pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,68 @@
-import type { NextPage } from 'next'
+import { promises as fs } from 'fs'
+import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
+import path from 'path'
 
-const Home: NextPage = () => {
+interface HomeStaticProps {
+  links: { title: string; link: string }[]
+}
+
+const dirToTitleCase = (str: string) => {
+  return str
+    .split('-')
+    .map((word: string) => {
+      const capitalLetter = word.charAt(0).toUpperCase()
+      return `${capitalLetter}${word.slice(1)}`
+    })
+    .join(' ')
+}
+
+const dirToLink = (dir: string) => ({ title: dirToTitleCase(dir), link: `examples/${dir}` })
+
+const getExampleLinks = async () => {
+  const examplesPath = path.join(process.cwd(), 'src/pages/examples')
+  const examplesPages = await fs.readdir(examplesPath)
+
+  return examplesPages.map(dirToLink)
+}
+
+export const getStaticProps: GetStaticProps<HomeStaticProps> = async () => {
+  const links = await getExampleLinks()
+
+  return {
+    props: {
+      links: links,
+    },
+  }
+}
+
+const Home: NextPage<HomeStaticProps> = ({ links }) => {
   return (
-    <div>
+    <>
       <Head>
         <title>WebGL Examples</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Home</h1>
-    </div>
+
+      <main style={{ padding: '2rem' }}>
+        <h1>Example Links</h1>
+
+        <section style={{ padding: '1rem 0' }}>
+          <ul style={{ listStyle: 'none' }}>
+            {links.map(({ title, link }) => {
+              return (
+                <li key={title}>
+                  <Link href={link}>
+                    <a>- {title}</a>
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+        </section>
+      </main>
+    </>
   )
 }
 
